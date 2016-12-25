29f5444d1f3fd68572423284e8fe861ef35dd397
Raymond Kristiansen <raymond@nexusweb.no>
2016-12-25 14:08:19 +0100

    first

---
+++ README.md
@@ -0,0 +1,10 @@
+## Development
+
+Use virtualenv with global site packages:
+
+```bash
+cd himlarcli
+virtualenv . --system-site-packages
+source bin/activate
+python setup.py develop
+
---
+++ setup.py
@@ -0,0 +1,19 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'description': 'Telldus and webcrux python cli',
+    'author': 'Raymond Kristiansen',
+    'url': 'https://github.com/raykrist/tellcrux',
+    'download_url': 'https://github.com/raykrist/tellcrux',
+    'author_email': 'raymond@nexusweb.no',
+    'version': '0.1',
+    'install_requires': [ 'tellcore-py' ],
+    'packages': ['tellcrux'],
+    'scripts': [],
+    'name': 'tellcrux'
+}
+
+setup(**config)
